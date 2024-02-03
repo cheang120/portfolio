bb0c27d6dfffa70885c9bd9962ad9092b19822b0
cheang120 <babycode.2023@gmail.com>
2024-02-03 23:26:09 +0800

    system

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -4,5 +4,5 @@ import { ReactNode } from "react";
 import { ThemeProvider } from "next-themes";
 
 export function Provider({children}:{children:ReactNode}) {
-    return <ThemeProvider attribute="class" defaultTheme="system ">{children}</ThemeProvider>
+    return <ThemeProvider attribute="class" >{children}</ThemeProvider>
 }
